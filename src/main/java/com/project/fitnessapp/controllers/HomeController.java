85299f57082c0796993f6d38b5d9458e5b685880
MilaVidoevska <milamila358@gmail.com>
2024-09-18 14:29:38 +0200

    First controller

---
+++ src/main/java/com/project/fitnessapp/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.project.fitnessapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, Spring Boot!";
+    }
+}
